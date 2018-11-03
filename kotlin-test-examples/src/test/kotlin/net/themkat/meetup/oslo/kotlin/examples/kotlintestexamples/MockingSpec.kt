0a7c2a65e559b6849779a0b41bb9c2413ed8625f
Marie Katrine Ekeberg <spacemarky@gmail.com>
2018-11-03 15:24:55 +0100

    Added a file we will do the mocking examples in.

---
+++ kotlin-test-examples/src/test/kotlin/net/themkat/meetup/oslo/kotlin/examples/kotlintestexamples/MockingSpec.kt
@@ -0,0 +1,17 @@
+package net.themkat.meetup.oslo.kotlin.examples.kotlintestexamples
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+// THis will show you the power of mocking with MockK!
+
+object MockingSpec : Spek({
+
+    describe("mock the environment controller so we don't have to start Spring") {
+
+        it("should return our mocked response") {
+
+        }
+
+    }
+})
